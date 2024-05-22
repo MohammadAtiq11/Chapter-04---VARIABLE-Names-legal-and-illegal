afac8e2228cd22482106043d6c6e58950f0b75b9
Mohammad Atiq <154668693+MohammadAtiq11@users.noreply.github.com>
2024-05-22 22:43:23 +0500

    Chapter-04---VARIABLE-Names-legal-and-illegal

---
+++ app.js
@@ -0,0 +1,43 @@
+// Q:1
+// A:1
+
+// let date = 11,
+//   month = 3,
+//   year = 1995;
+// console.log(date, month, year);
+
+// Q:2
+// A:2
+
+// 5 legal variable names
+
+// let myname = atiq;
+// let $myname = atiq;
+// let my_name = atiq;
+// let MyName = atiq;
+// let Myname11 = atiq;
+
+// 5 illegal variable names
+
+// let my name = atiq;
+// let @myname = atiq
+// let 11myname = atiq
+// let my-name = atiq
+// let var = atiq
+
+// Q:3
+// A:3
+
+// let a = "<h1>Rules for naming JS variables</h1>";
+// let b =
+//   "<b>Variable names can only contain , numbers, $ and _ . For example $my_1stVariable</b>";
+// let c =
+//   "<b>Variables must begin with a letter, $ or _ .For example $name, _name or name</b>";
+// let d = "<b>Variable names are case sensitive</b>";
+// let e = "<b>Variable names should not be JS keywords</b>";
+
+// document.write(a, "<br/><br/><br/>");
+// document.write(b, "<br/>");
+// document.write(c, "<br/>");
+// document.write(d, "<br/>");
+// document.write(e);
